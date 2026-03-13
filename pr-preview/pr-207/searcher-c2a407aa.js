1b31f4f988fbc2df726376b5b469942ab18b69ac
mrmans0n <mrmans0n@users.noreply.github.com>
2026-03-13 10:29:38 +0000

    Deploy preview for PR 207 🛫

--- pr-preview/pr-207/searcher-c2a407aa.js
+++ pr-preview/pr-207/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-66577ee6.js',
+                path_to_root + 'searchindex-434c1676.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');

2b793e4a751c9ee634220b5fef3e17d18c3884f4
mrmans0n <mrmans0n@users.noreply.github.com>
2026-07-21 08:05:58 +0000

    Deploy preview for PR 394 🛫

--- pr-preview/pr-394/searcher-09f2665d.js
+++ pr-preview/pr-394/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-022e3194.js',
+                path_to_root + 'searchindex-a954c1b5.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');

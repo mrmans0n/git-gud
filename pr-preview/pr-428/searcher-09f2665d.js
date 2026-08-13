c760e91d23322ad2f0817997758fba20465a1e48
mrmans0n <mrmans0n@users.noreply.github.com>
2026-08-13 13:08:29 +0000

    Deploy preview for PR 428 🛫

--- pr-preview/pr-428/searcher-09f2665d.js
+++ pr-preview/pr-428/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-18d5df9d.js',
+                path_to_root + 'searchindex-f2f8e710.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
